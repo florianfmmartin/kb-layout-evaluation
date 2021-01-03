b7aa6b545ae2b2d6e44a5a8505610a2026af30f5
florianfmmartin <florian.fm.martin@gmail.com>
2021-01-03 15:34:49 -0500

    Added features and prompts

--- layout_evaluation/script.py
+++ layout_evaluation/script.py
@@ -5,7 +5,6 @@ import re
 import sys
 import pandas as pd
 from io import StringIO
-import matplotlib.pyplot as plt
 
 
 def main():
@@ -13,22 +12,41 @@ def main():
     print("Welcome to the Keyboard Layout Evaluator & Generator:")
     eval_or_gen = input("Do you want to [e]valuate or [g]enerate? [e/g]: ")
 
+    langs = input("""For what languages do you want to generate a layout?
+Ex. with all: en fr es de
+Yours: """).split(" ")
+
+
+    langs_dict = {}
+
+    for lang in langs:
+        if lang not in ["fr", "en", "de", "es"]:
+            print("Error in language input! Closing...")
+            exit()
+        else:
+            langs_dict[lang] = int(input(f"Weight for {lang} (int): "))
+
+    weight_name = "w_" + input("Name of weights to use [colemak/beakl/darn]: ")
+
     if eval_or_gen == "e":
-        app_evaluate()
+        top_show = int(input("Show top [x] results: "))
+        print()
+        app_evaluate(langs_dict, top_show, weight_name)
     elif eval_or_gen == "g":
-        app_generate()
+        app_generate(langs_dict, weight_name)
     else:
         print("Bibop! I don't know what to do! Closing...")
         quit()
 
 
-def app_evaluate():
+def app_evaluate(langs_dict, top_show, weight_name):
     """
     Evaluates layout in config.txt.
     This was implemented in github.com/bclnr/kb-layout-evaluation
     """
     # load the blocks from config.txt and parse them into dataframes
-    df_layouts, df_keys, df_bigrams, df_penalties, keys, blocks = parse_config(load_config(), "e")
+    df_layouts, df_keys, df_bigrams, df_penalties, keys, blocks = parse_config(
+                                                                    load_config(weight_name), "e")
 
     # as some letters are not present in all layouts, they can be manually removed from the bigrams list
     letters_to_ignore = 'êàçâîôñäöüß/'
@@ -49,8 +67,10 @@ def app_evaluate():
     for row in df_bigrams.itertuples():
         for c in punctuation:
             if str(c) in row.Index:
-                df_bigrams.at[row.Index, 'en'] = df_bigrams.at[row.Index, 'en_perso']
-                df_bigrams.at[row.Index, 'fr'] = df_bigrams.at[row.Index, 'fr_perso']
+                df_bigrams.at[row.Index,
+                              'en'] = df_bigrams.at[row.Index, 'en_perso']
+                df_bigrams.at[row.Index,
+                              'fr'] = df_bigrams.at[row.Index, 'fr_perso']
 
     # normalize df_bigrams to get 100% on each column
     df_bigrams = df_bigrams * 100 / df_bigrams.sum(axis=0)
@@ -64,51 +84,61 @@ def app_evaluate():
         print(df_missing_letters)
 
     # generate a dataframe of the weights per bigram per layout
-    df_bigram_weight = bigram_weight(df_layouts, df_keys, df_bigrams, df_penalties)
+    df_bigram_weight = bigram_weight(
+        df_layouts, df_keys, df_bigrams, df_penalties)
 
     # get the results
     df_results = layout_results(df_bigrams, df_bigram_weight)
 
     # add average column with arbitrary coefs per language
-    # my average of 30/70 en fr
-    df_results['Personal average'] = df_results.en * 0.6 + df_results.fr * 0.4
+    w_en = 0
+    w_fr = 0
+    w_de = 0
+    w_es = 0
+
+    w_total = 0
+
+    for w in langs_dict.values():
+        w_total += w
+
+    for name, weight in langs_dict.items():
+        if name == "fr":
+            w_fr = weight / w_total
+        elif name == "en":
+            w_en = weight / w_total
+        elif name == "de":
+            w_de = weight / w_total
+        elif name == "es":
+            w_es = weight / w_total
+
+    df_results['Personal average'] = (df_results.en * w_en +
+                                      df_results.fr * w_fr +
+                                      df_results.de * w_de +
+                                      df_results.es * w_es)
 
     # normalize the results based of Qwerty Personnal average
-    df_results = df_results.applymap(lambda x: round(x/df_results.at['Qwerty', 'Personal average'] * 100, 2))
-    # df_results = df_results.applymap(lambda x: round(x/df_results.at['Qwerty', 'en'] * 100, 2))
+    df_results = df_results.applymap(lambda x: round(
+        x/df_results.at['Qwerty', 'Personal average'] * 100, 2))
 
     # sort the results
-    # df_results = df_results.sort_values(by=['en'], ascending=True)
-    df_results = df_results.sort_values(by=['Personal average'], ascending=True)
+    df_results = df_results.sort_values(
+        by=['Personal average'], ascending=True)
 
     # filter/reorder the results
-    df_results = df_results[['Personal average', 'en', 'en_perso', 'fr', 'fr_perso', 'es', 'de']] # results_full.png
+    df_results = df_results[['Personal average', 'en', 'fr', 'es', 'de']]
 
-    print(df_results)
-
-
-def layout_list_to_str(name, layout_list):
-    layout_str = f">>{name}\n"
-    index = 1
-    for symbol in layout_list:
-        if index % 12 == 0:
-            layout_str += f"{symbol}\n"
-        else:
-            layout_str += f"{symbol} "
-        index += 1
-
-    layout_str += "\n"
-    return layout_str
+    print(df_results.iloc[:top_show])
 
 
-def app_generate():
+def app_generate(langs_dict, weight_name):
     """
     Generates layout based on swapping symbols
     from the first layout in config.txt until
     no swap can make the ergonomic score lower.
     """
     # load data
-    df_layouts, df_keys, df_bigrams, df_penalties, keys, blocks = parse_config(load_config(), "g")
+    df_layouts, df_keys, df_bigrams, df_penalties, keys, blocks = parse_config(
+        load_config(weight_name), "g")
 
     # as some letters are not present in all layouts, they can be manually removed from the bigrams list
     letters_to_ignore = 'êàçâîôñäöüß/'
@@ -129,8 +159,10 @@ def app_generate():
     for row in df_bigrams.itertuples():
         for c in punctuation:
             if str(c) in row.Index:
-                df_bigrams.at[row.Index, 'en'] = df_bigrams.at[row.Index, 'en_perso']
-                df_bigrams.at[row.Index, 'fr'] = df_bigrams.at[row.Index, 'fr_perso']
+                df_bigrams.at[row.Index,
+                              'en'] = df_bigrams.at[row.Index, 'en_perso']
+                df_bigrams.at[row.Index,
+                              'fr'] = df_bigrams.at[row.Index, 'fr_perso']
 
     # normalize df_bigrams to get 100% on each column
     df_bigrams = df_bigrams * 100 / df_bigrams.sum(axis=0)
@@ -141,11 +173,13 @@ def app_generate():
     best_str = ""
     current_str = ""
     best_name = ""
-    current_name = "BEAKL 19bis"
+    current_name = "Qwerty"
     times = 0
+    limit = 4
 
-    while(current_score < best_score and times < 8):
-        times += 1
+    while(current_score < best_score and times < limit):
+        # un comment to limit
+        # times += 1
 
         # if enter the loop then best score is current_score
         best_score = current_score
@@ -154,7 +188,8 @@ def app_generate():
 
         # if not first iteration
         if current_score != 100:
-            df_layouts = create_df_layouts(keys, create_layouts_str_for_swap(best_name, blocks))
+            df_layouts = create_df_layouts(
+                keys, create_layouts_str_for_swap(best_name, blocks))
 
         # this prints letters present in bigrams but not in a layout
         # letters absent from a layout do not count in the grade
@@ -165,20 +200,40 @@ def app_generate():
             print(df_missing_letters)
 
         # generate a dataframe of the weights per bigram per layout
-        df_bigram_weight = bigram_weight(df_layouts, df_keys, df_bigrams, df_penalties)
+        df_bigram_weight = bigram_weight(
+            df_layouts, df_keys, df_bigrams, df_penalties)
 
         # get the results
         df_results = layout_results(df_bigrams, df_bigram_weight)
 
         # add average column with arbitrary coefs per language
-        # my average of 30/70 en fr
-        df_results['Personal average'] = df_results.en * 0.4 + df_results.fr * 0.6
+        w_en = 0
+        w_fr = 0
+        w_de = 0
+        w_es = 0
+
+        for name, weight in langs_dict.items():
+            if name == "fr":
+                w_fr = weight
+            elif name == "en":
+                w_en = weight
+            elif name == "de":
+                w_de = weight
+            elif name == "es":
+                w_es = weight
+
+        df_results['Personal average'] = (df_results.en * w_en +
+                                          df_results.fr * w_fr +
+                                          df_results.de * w_de +
+                                          df_results.es * w_es)
 
         # normalize the results based of Qwerty Personnal average
-        df_results = df_results.applymap(lambda x: round(x/df_results.at['Qwerty', 'Personal average'] * 100, 2))
+        df_results = df_results.applymap(lambda x: round(
+            x/df_results.at['Qwerty', 'Personal average'] * 100, 2))
 
         # sort the results
-        df_results = df_results.sort_values(by=['Personal average'], ascending=True)
+        df_results = df_results.sort_values(
+            by=['Personal average'], ascending=True)
 
         # filter/reorder the results
         df_results = df_results[['Personal average']]
@@ -186,7 +241,8 @@ def app_generate():
         best_layout_name = df_results.index[0]
         best_layout_score = df_results.to_numpy()[0][0]
         best_layout_layout_list = df_layouts.get(best_layout_name).to_numpy()
-        best_layout_as_str = layout_list_to_str(best_layout_name, best_layout_layout_list)
+        best_layout_as_str = layout_list_to_str(
+            best_layout_name, best_layout_layout_list)
 
         current_score = best_layout_score
         current_str = best_layout_as_str
@@ -198,52 +254,82 @@ def app_generate():
     print(best_str)
 
 
-def load_config():
+def layout_list_to_str(name, layout_list):
+    """
+    Utility function to format layout as list into a string to feed to the evaluation.
+    """
+    layout_str = f">>{name}\n"
+    index = 1
+    for symbol in layout_list:
+        if index % 12 == 0:
+            layout_str += f"{symbol}\n"
+        else:
+            layout_str += f"{symbol} "
+        index += 1
+
+    layout_str += "\n"
+    return layout_str
+
+
+def load_config(weight_name):
     """
     Load the config file and outputs its content as a list of tuples (title, block)
     """
-    
+
     # load the whole file into a str
-    filepath = os.path.join(os.path.dirname(os.path.realpath('__file__')), 'config.txt')
+    filepath = os.path.join(os.path.dirname(
+        os.path.realpath('__file__')), 'config.txt')
     filehandle = open(filepath)
     filetext = filehandle.read()
-    
+
     # remove the comments from the file
     filetext = removeComments(filetext)
-    
+
     # find all the paragraphs
     parts = re.findall(r'\[(.*?)\]', filetext)
-    
+
     # to cut into blocks
     parts_location = []
     blocks = []
     # find the paragraphs location, put it in a tuple (start, end) of title
     for t in parts:
         part_location = filetext.find('[' + t + ']')
-        parts_location.append((part_location, filetext.find('\n', part_location) + 1))
+        parts_location.append(
+            (part_location, filetext.find('\n', part_location) + 1))
     # put paragraphs into blocks list
     for i in range(len(parts_location)):
         if i < len(parts_location) - 1:
-            blocks.append(filetext[parts_location[i][1]:parts_location[i + 1][0]])
+            blocks.append(filetext[parts_location[i][1]
+                          :parts_location[i + 1][0]])
         else:
             blocks.append(filetext[parts_location[i][1]:])
-    
+
     # remove blank lines from the blocks
     for i in range(len(blocks)):
         blocks[i] = os.linesep.join([s for s in blocks[i].splitlines() if s])
-    
+
     # return the blocks
     output = []
     for i in range(len(blocks)):
         output.append((parts[i], blocks[i]))
-    return output
+
+    output_w = []
+
+    for title, block in output:
+        if "w_" in title:
+            if title == weight_name:
+                output_w.append(("weights", block))
+        else:
+            output_w.append((title, block))
+
+    return output_w
 
 
 def parse_config(blocks, eval_or_gen):
     """
     Takes the list of tuples (title, block), and outputs the dataframes
     """
-    
+
     # check that all the needed configuration is present
     blocks_needed = ['keys', 'weights', 'penalties', 'layouts']
     blocks_available = []
@@ -253,14 +339,14 @@ def parse_config(blocks, eval_or_gen):
         if t not in blocks_available:
             print('Missing block from config file: ' + t)
             sys.exit()
-    
+
     # find the location of the keys in the blocks list
     for i in range(len(blocks)):
         if blocks[i][0] == 'keys':
             break
     # create list of keys
     keys = blocks[i][1].split()
-    
+
     # find the location of the weights in the blocks list
     for i in range(len(blocks)):
         if blocks[i][0] == 'weights':
@@ -305,18 +391,21 @@ def parse_config(blocks, eval_or_gen):
     if eval_or_gen == "e":
         df_layouts = create_df_layouts(keys, blocks[i][1])
     else:
-        df_layouts = create_df_layouts(keys, create_layouts_str_for_swap(name_block, blocks[i][1]))
+        df_layouts = create_df_layouts(
+            keys, create_layouts_str_for_swap(name_block, blocks[i][1]))
     # df_layouts is a dataframe of all predefined layouts to evaluate
 
     # dataframe of bigrams by language
-    df_bigrams = pd.read_csv(os.path.join(os.path.dirname(os.path.realpath('__file__')), 'stats.csv'), header=0, sep=',', index_col=0)
+    df_bigrams = pd.read_csv(os.path.join(os.path.dirname(
+        os.path.realpath('__file__')), 'stats.csv'), header=0, sep=',', index_col=0)
     # df_bigrams is a dataframe of all possible bigrams (aa, ab…) with their probability per language
 
     # find the location of the penalties in the blocks list
     for i in range(len(blocks)):
         if blocks[i][0] == 'penalties':
             break
-    df_penalties = pd.read_csv(StringIO(blocks[i][1]), sep=',', header=0, index_col=0, skipinitialspace=True)
+    df_penalties = pd.read_csv(
+        StringIO(blocks[i][1]), sep=',', header=0, index_col=0, skipinitialspace=True)
     # df_penalties is a dataframe of the penalties to add to bigrams, by finger and row jump
 
     return df_layouts, df_keys, df_bigrams, df_penalties, keys, blocks[i][1]
@@ -328,7 +417,7 @@ def check_missing_letters(df_layouts, df_bigrams):
     """
     # create empty dataframe
     df_missing_letters = pd.DataFrame(index=df_layouts.keys())
-    
+
     # get the list of bigrams
     bigram_list = list(df_bigrams.index.values)
     # iterate to compile list of letters
@@ -338,17 +427,19 @@ def check_missing_letters(df_layouts, df_bigrams):
             letters_list.append(i[0])
         if i[1] not in letters_list:
             letters_list.append(i[1])
-    
+
     # iterate over the layouts
     for layout in df_missing_letters.itertuples():
         # iterate over letters
         missing_letters = ""
         for i in letters_list:
             if i not in df_layouts[layout.Index].values:
+                print(layout)
                 missing_letters = missing_letters + i
         if missing_letters != '':
-            df_missing_letters.at[layout.Index, 'Missing letters'] = missing_letters
-    
+            df_missing_letters.at[layout.Index,
+                                  'Missing letters'] = missing_letters
+
     return df_missing_letters
 
 
@@ -393,11 +484,12 @@ def make_swap_layouts(base_name, base_layout):
     # symbols to swap
     symbols_to_swap = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
                        "a", "s", "d", "f", "g", "h", "j", "k", "l", "z",
-                       "x", "c", "v", "b", "n", "m", ",", ".", "'", "é"]
+                       "x", "c", "v", "b", "n", "m"]
 
     # base_layout extraction
     base_layout = base_layout.split("\n")
-    base_layout = base_layout[1] + "\n" + base_layout[2] + "\n" + base_layout[3]
+    base_layout = base_layout[1] + "\n" + \
+        base_layout[2] + "\n" + base_layout[3]
 
     all_layouts = ""
     combination_tried = []
@@ -407,7 +499,8 @@ def make_swap_layouts(base_name, base_layout):
         for symbol_to in symbols_to_swap:
             # checks if same symbol or already swapped
             same_symbol = symbol_from == symbol_to
-            tried = (symbol_from, symbol_to) in combination_tried or (symbol_to, symbol_from) in combination_tried
+            tried = (symbol_from, symbol_to) in combination_tried or (
+                symbol_to, symbol_from) in combination_tried
 
             if not same_symbol and not tried:
                 # adds name
@@ -433,7 +526,7 @@ def weight(bigram, layout, df_layouts, df_keys, df_penalties):
     """
     Returns the calculated weight of a bigram, for a given layout
     """
-    
+
     # check that the bigram keys exist in the layout, otherwise return 0
     if (bigram[0] not in df_layouts[layout].values) or (bigram[1] not in df_layouts[layout].values):
         return 0.0
@@ -461,9 +554,9 @@ def weight(bigram, layout, df_layouts, df_keys, df_penalties):
             rowjump = "row_jump2"
         else:
             sys.exit('Penalty for line jump not defined')
-        
+
         penalty = df_penalties.at[finger1 + finger2, rowjump]
-    
+
     return weight1 + weight2 + penalty
 
 
@@ -472,13 +565,15 @@ def bigram_weight(df_layouts, df_keys, df_bigrams, df_penalties):
     Create a dafaframe of the weight per bigram and layout
     """
     # create the empty dataframe
-    df_bigram_weight = pd.DataFrame(index=df_bigrams.index.values, columns=df_layouts.keys())
-    
+    df_bigram_weight = pd.DataFrame(
+        index=df_bigrams.index.values, columns=df_layouts.keys())
+
     # iterate over the whole dataframe to compute the weights
     # iterating isn't efficient but I don't know better
     for column in df_bigram_weight:
         for row in df_bigram_weight.itertuples():
-            df_bigram_weight.at[row.Index, column] = weight(row.Index, column, df_layouts, df_keys, df_penalties)
+            df_bigram_weight.at[row.Index, column] = weight(
+                row.Index, column, df_layouts, df_keys, df_penalties)
 
     return df_bigram_weight
 
@@ -488,14 +583,16 @@ def layout_results(df_bigrams, df_bigram_weight):
     Generate dataframe of results (grade per layout per language)
     """
     # create the empty dataframe
-    df_results = pd.DataFrame(index=df_bigram_weight.keys(), columns=df_bigrams.keys())
-    
+    df_results = pd.DataFrame(
+        index=df_bigram_weight.keys(), columns=df_bigrams.keys())
+
     # iterate over the dataframe to compute the grades
     # iterating isn't efficient but this is small enough
-    for column in df_results: # language
-        for row in df_results.itertuples(): # layout
+    for column in df_results:  # language
+        for row in df_results.itertuples():  # layout
             # sum of (probability of bigram (from df_bigram, is a percentage) times its weight)
-            df_results.at[row.Index, column] = (df_bigrams[column]/100 * df_bigram_weight[row.Index]).sum()
+            df_results.at[row.Index, column] = (
+                df_bigrams[column]/100 * df_bigram_weight[row.Index]).sum()
 
     return df_results
 
@@ -507,13 +604,13 @@ def removeComments(string):
     """
     pattern = r"(\".*?\"|\'.*?\')|(/\*.*?\*/|//[^\r\n]*$)"
     regex = re.compile(pattern, re.MULTILINE | re.DOTALL)
-    
+
     def _replacer(match):
         if match.group(2) is not None:
             return ""
         else:
             return match.group(1)
-    
+
     return regex.sub(_replacer, string)
 
 
